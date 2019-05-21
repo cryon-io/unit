a706146ecb4fb8473741185402cb3b2f147cbf45
cryi <cryi@tutanota.com>
2019-05-21 15:54:59 +0200

    switch back to sbin

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 LATEST=$(curl -sL https://api.github.com/repos/cryon-io/unit/releases/latest | grep tag_name | sed 's/  "tag_name": "//g' | sed 's/",//g')
 wget https://github.com/cryon-io/unit/releases/download/$LATEST/unit-linux-x64.zip -O unit.zip &&
     unzip -o unit.zip &&
-    mv unit-linux-x64 /usr/bin/unit &&
-    chmod +x /usr/bin/unit &&
+    mv unit-linux-x64 /usr/sbin/unit &&
+    chmod +x /usr/sbin/unit &&
     mkdir -p /etc/unit/ &&
     [ ! -f "/etc/unit/unit.json" ] && printf "{
         
