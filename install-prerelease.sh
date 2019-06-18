89f988c449a9a9a8e185a094852e33915e94395c
cryi <cryi@tutanota.com>
2019-06-18 22:27:43 +0200

    updated install scripts

--- install-prerelease.sh
+++ install-prerelease.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 LATEST=$(curl -sL https://api.github.com/repos/cryon-io/unit/releases | grep tag_name | head -n 1 | sed 's/  "tag_name": "//g' | sed 's/",//g' | sed 's/ //g')
+
+command -v apt-get && apt-get update && apt-get install -y -q unzip
+command -v dnf && dnf -y install unzip
+command -v yum && yum install -y unzip
+
 wget https://github.com/cryon-io/unit/releases/download/$LATEST/unit-linux-x64.zip -O unit.zip &&
     unzip -o unit.zip &&
     mv unit-linux-x64 /usr/sbin/unit &&
